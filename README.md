62222df9ba3772117b6a09153b4b9b3290f75aca
Elias Benbourenane <eliasbenbourenane@gmail.com>
2020-08-03 22:01:20 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# HerokuShell-Rclone
+ Interface Heroku's shell through Telegram, plus Rclone support
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Install rclone static binary
+wget -q https://downloads.rclone.org/v1.51.0/rclone-v1.51.0-linux-amd64.zip
+unzip -q rclone-v1.51.0-linux-amd64.zip
+export PATH=$PWD/rclone-v1.51.0-linux-amd64:$PATH
