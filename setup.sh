3044269ce9c0861578c6ce831c79734e26048147
mohitjoshi155 <58655292+mohitjoshi155@users.noreply.github.com>
2021-04-24 15:52:59 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Install rclone static binary
+curl https://rclone.org/install.sh |  bash -s beta
 wget -q https://downloads.rclone.org/v1.52.3/rclone-v1.52.3-linux-amd64.zip
 unzip -q rclone-v1.52.3-linux-amd64.zip
 export PATH=$PWD/rclone-v1.52.3-linux-amd64:$PATH
