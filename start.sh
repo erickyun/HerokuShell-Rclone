371e9bb03c2f818263ddd9b6798147267f4e442f
erickyun <71968711+erickyun@users.noreply.github.com>
2022-11-26 11:16:44 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 if [[ -n $RCLONE_CONFIG_BASE64 ]]; then
 	echo "Rclone config detected"
 	echo "$(echo $RCLONE_CONFIG_BASE64|base64 -d)" >> rclone.conf
-    mkdir -p /app/.config/rclone
+    sudo mkdir -p /app/.config/rclone
 	echo "[DRIVE]" > /app/.config/rclone/rclone.conf
 fi
 
